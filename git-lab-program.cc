986bf05b9f5b5b499a328f117907b50b45daeee0
jbrennan35 <97566039+jbrennan35@users.noreply.github.com>
2022-01-18 19:33:11 -0500

    Create git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: James Brennan
+ *        Date: January 18, 2022
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
